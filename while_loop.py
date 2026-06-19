a9738f8610e1bb877cdfb4fc6e446d6887d38e02
arun-kumar-2141 <choudharyarun2141@gmail.com>
2026-06-19 16:11:51 +0530

    initial commit

---
+++ while_loop.py
@@ -0,0 +1,17 @@
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
+
+
+n = int(input("enter your no"))
+i = 1
+while i <= 10:
+    print(i*n)
+    i += 1
